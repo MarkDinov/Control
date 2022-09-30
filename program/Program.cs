19b3988fb3801aed3e2635d7d2115a41b8333b24
Mark <mutofifin@mail.ru>
2022-09-30 19:46:15 +0300

    код программа

---
+++ program/Program.cs
@@ -0,0 +1,12 @@
+﻿string[]array = {"day", "manth", "if"};
+int i = 0; 
+int k = 3;
+int n = array.Length;
+while (i < n)
+{
+    if (array[i].Length <= k)
+    {
+        Console.Write(array[i] + " ");
+    }
+    i= i + 1;
+}
